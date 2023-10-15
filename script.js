bf8e2b1b70645176ae873f31a649a8413ca6ff2f
Aaron Finazzo <finazzocodes@gmail.com>
2023-10-15 15:51:06 -0400

    Complete js functionality
    This commit adds the JavaScript code required for the Etch-a-Sketch
    application. The code handles user interactions, drawing on the grid,
    changing colors, modes, and grid size, as well as clearing the grid.

--- script.js
+++ script.js
@@ -6,31 +6,93 @@ let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
 
-function setCurrentColor(newColor) {
+let setCurrentColor = (newColor) => {
   currentColor = newColor;
-}
+};
 
-function setCurrentMode(newMode) {
+let setCurrentMode = (newMode) => {
   activateButton(newMode);
   currentMode = newMode;
-}
+};
 
-function setCurrentSize(newSize) {
+let setCurrentSize = (newSize) => {
   currentSize = newSize;
-}
+};
 
-const gridContainer = document.querySelector(".grid-container");
+const colorPicker = document.getElementById("colorPicker");
+const colorBtn = document.getElementById("colorBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("clearBtn");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const gridContainer = document.getElementById("grid-container");
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode("color");
+eraserBtn.onclick = () => setCurrentMode("eraser");
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+let changeSize = (value) => {
+  setCurrentSize(value);
+  updateSizeValue(value);
+  reloadGrid();
+};
+
+let updateSizeValue = (value) => {
+  sizeValue.innerHTML = `${value} * ${value}`;
+};
+
+let reloadGrid = () => {
+  clearGrid();
+  createGrid(currentSize);
+};
+
+let clearGrid = () => {
+  gridContainer.innerHTML = "";
+};
 
 let createGrid = (size) => {
   for (i = 0; i < size * size; i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("grid-cell");
+    gridCell.style.width = `calc(100% / ${size})`;
+    gridCell.style.height = `calc(100% / ${size})`;
+    gridCell.addEventListener("mouseover", changeColor);
+    gridCell.addEventListener("mousedown", changeColor);
     gridContainer.appendChild(gridCell);
-
-    gridCell.addEventListener("mouseenter", () => {
-      gridCell.style.backgroundColor = "green";
-    });
   }
 };
 
-createGrid(currentColor);
+let changeColor = (e) => {
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (currentMode === "color") {
+    e.target.style.backgroundColor = currentColor;
+  } else if (currentMode === "eraser") {
+    e.target.style.backgroundColor = "#fefefe";
+  }
+};
+
+let activateButton = (newMode) => {
+  if (currentMode === "color") {
+    colorBtn.classList.remove("active");
+  } else if (currentMode === "eraser") {
+    eraserBtn.classList.remove("active");
+  }
+
+  if (newMode === "color") {
+    colorBtn.classList.add("active");
+  } else if (newMode === "eraser") {
+    eraserBtn.classList.add("active");
+  }
+};
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE);
+  activateButton(DEFAULT_MODE);
+};
